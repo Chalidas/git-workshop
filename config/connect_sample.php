387dc58c5502ea58d75138d80cd6c28972c526c6
Chalida BO <sanviseadchalida@gmail.com>
2021-05-08 17:10:23 +0700

    First Commit

---
+++ config/connect_sample.php
@@ -0,0 +1,9 @@
+<?php
+ $db_server="localhost";
+ $db_user="youruser";
+ $db_pass="yourpass";
+ $db_database="yourdb";
+ mysqli_connect($db_server,$db_user,$db_pass,$db_database);
+
+echo "Connect DB";
+?>
